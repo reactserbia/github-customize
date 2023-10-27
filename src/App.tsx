a40f998919a34605d8f85fac09e099640cecd6d9
Mirko Basic <bejzik8@gmail.com>
2023-10-27 19:04:33 +0200

    Added get access token functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
     const [code, setCode] = useState('')
+    const [accessToken, setAccessToken] = useState('')
 
     useEffect(() => {
         const queryString = window.location.search
@@ -22,29 +24,75 @@ function App() {
         )
     }
 
+    const getAccessToken = async () => {
+        try {
+            const { data } = await axios.post(
+                'http://localhost:3000/github/get-access-token',
+                {
+                    code
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+
+            console.log(data)
+
+            if (data.response?.access_token) {
+                setAccessToken(data.response.access_token)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     console.log(code)
+    console.log(accessToken)
 
     return (
         <div>
-            <h1>GitHub Customize</h1>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <div className='card'>
-                <button onClick={loginWithGitHub}>Login with GitHub</button>
-                <p className='read-the-docs'>
-                    Login with GitHub in order to enter the application.
-                </p>
-            </div>
+            {(() => {
+                if (accessToken) return <p>Access Token: {accessToken}</p>
+                if (code)
+                    return (
+                        <button onClick={getAccessToken}>
+                            Get Access Token
+                        </button>
+                    )
+
+                return (
+                    <>
+                        <h1>GitHub Customize</h1>
+                        <div>
+                            <a href='https://vitejs.dev' target='_blank'>
+                                <img
+                                    src={viteLogo}
+                                    className='logo'
+                                    alt='Vite logo'
+                                />
+                            </a>
+                            <a href='https://react.dev' target='_blank'>
+                                <img
+                                    src={reactLogo}
+                                    className='logo react'
+                                    alt='React logo'
+                                />
+                            </a>
+                        </div>
+                        <div className='card'>
+                            <button onClick={loginWithGitHub}>
+                                Login with GitHub
+                            </button>
+                            <p className='read-the-docs'>
+                                Login with GitHub in order to enter the
+                                application.
+                            </p>
+                        </div>
+                    </>
+                )
+            })()}
         </div>
     )
 }
