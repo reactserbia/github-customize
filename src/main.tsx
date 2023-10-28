719b1ed70f4420831b6282a752a3ef97b0531452
Mirko Basic <bejzik8@gmail.com>
2023-10-28 02:42:35 +0200

    Updated main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import './reset.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import { Layout } from './providers/Layout'
+
+// import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Layout>{/* <App /> */}</Layout>
+    </React.StrictMode>
 )
