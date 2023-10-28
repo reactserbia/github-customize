84b39ece795b241c310c5fce4c037177503908c4
Mirko Basic <bejzik8@gmail.com>
2023-10-28 02:41:08 +0200

    Created container CSS class.

---
+++ src/providers/container.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css'
+
+export const container = style({
+    height: 50,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+})
