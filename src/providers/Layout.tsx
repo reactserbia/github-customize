841294e8878066b82ecd9d1661ad9e283270acbd
Mirko Basic <bejzik8@gmail.com>
2023-10-28 02:41:20 +0200

    Created Layout provider.

---
+++ src/providers/Layout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useEffect, useState } from 'react'
+
+import { container } from './container.css'
+import { light, dark } from '../vars.css'
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+    const [themeIsLight, setThemeIsLight] = useState(true)
+
+    useEffect(() => {
+        document.querySelector('html')!.className = themeIsLight ? light : dark
+    }, [themeIsLight])
+
+    const toggleTheme = () => setThemeIsLight(theme => !theme)
+
+    return (
+        <div>
+            <div className={container}>
+                <button onClick={toggleTheme}>🌗</button>
+            </div>
+            {children}
+        </div>
+    )
+}
