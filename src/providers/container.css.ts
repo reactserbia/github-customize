9e3be0cdd15947fb6cf8d484a55b612ab6f6000d
Mirko Basic <bejzik8@gmail.com>
2023-10-28 02:56:14 +0200

    Added MAX_WIDTH constant.

--- src/providers/container.css.ts
+++ src/providers/container.css.ts
@@ -1,9 +1,11 @@
 import { style } from '@vanilla-extract/css'
+import { vars } from '../vars.css'
 
 export const container = style({
     height: 50,
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: vars.backgroundColor
 })
