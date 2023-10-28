435f31a1d7bf2f399a4a3cbe530573f0f5054b19
Mirko Basic <bejzik8@gmail.com>
2023-10-28 12:49:28 +0200

    React Router DOM configured.

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,46 @@ import './styles/reset.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Layout } from './providers/layout'
-import { LoginGuard } from './providers/login-guard'
 
 import { Home } from './routes/home'
+import { GitHubLogin } from './routes/github-login'
+import { LinkedInLogin } from './routes/linkedin-login'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: 'app',
+        children: [
+            {
+                path: 'login',
+                children: [
+                    {
+                        path: 'github',
+                        element: <GitHubLogin />
+                    },
+                    {
+                        path: 'linkedin',
+                        element: <LinkedInLogin />
+                    }
+                ]
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Layout>
-            <LoginGuard>
+            <RouterProvider router={router} />
+            {/* <LoginGuard>
                 <Home />
-            </LoginGuard>
+            </LoginGuard> */}
         </Layout>
     </React.StrictMode>
 )
