7f51fd9c2646c0ef5b69cf59c80bec095ca99346
Mirko Basic <bejzik8@gmail.com>
2023-10-28 10:50:19 +0200

    Updated main.ts.

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import './styles/reset.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Layout } from './providers/Layout'
+import { Layout } from './providers/layout1'
+import { LoginGuard } from './providers/login-guard'
 
-import App from './App.tsx'
+import { Home } from './routes/home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Layout>
-            <App />
+            <LoginGuard>
+                <Home />
+            </LoginGuard>
         </Layout>
     </React.StrictMode>
 )
