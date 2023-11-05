ae11472f58f8d0b148225406ae359c681cfebca3
Mirko Basic <bejzik8@gmail.com>
2023-11-05 03:24:37 +0100

    Added reordering functionality.

--- src/routes/app/app.css.ts
+++ src/routes/app/app.css.ts
@@ -1,5 +1,14 @@
+import { vars } from '@/styles/vars.css'
 import { style } from '@vanilla-extract/css'
 
 export const container = style({
     width: '100%'
+})
+
+export const reorderItem = style({
+    width: '100%',
+    backgroundColor: vars.elementBackground,
+    borderRadius: '4px',
+    padding: '1rem',
+    marginBottom: '1rem'
 })
--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -1,9 +1,22 @@
-import './app.css'
+import { useState } from 'react'
+import { Reorder } from 'framer-motion'
+
+import { ReorderItem } from '@/components'
+
+import { container } from './app.css'
 
 export function App() {
+    const [items, setItems] = useState(['1', '2', '3', '4', '5'])
+
     return (
-        <div>
+        <div className={container}>
             <p>WELCOME TO GITHUB ENHANCER!</p>
+
+            <Reorder.Group axis='y' values={items} onReorder={setItems}>
+                {items.map(item => (
+                    <ReorderItem key={item} item={item} />
+                ))}
+            </Reorder.Group>
         </div>
     )
 }
