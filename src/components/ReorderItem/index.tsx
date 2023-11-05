5233d2cb6a1cb46ad7a5bd8f5efd9809f1a2e826
Mirko Basic <bejzik8@gmail.com>
2023-11-05 05:10:48 +0100

    Updated ReorderItem component.

--- src/components/ReorderItem/index.tsx
+++ src/components/ReorderItem/index.tsx
@@ -1,20 +1,43 @@
+import { ChangeEvent, useRef, useState } from 'react'
 import { Reorder } from 'framer-motion'
 import { FormControl, FormField } from '@radix-ui/react-form'
+import { RowSpacingIcon } from '@radix-ui/react-icons'
 
 import { Element } from '@/models'
 
-import { container } from './reorder-item.css'
+import { container, dragHandle, formField, textarea } from './reorder-item.css'
 
 type ReorderItemProps = {
     item: Element
 }
 
 export const ReorderItem = ({ item }: ReorderItemProps) => {
+    const [value, setValue] = useState('')
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+    const onChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        if (!textareaRef.current) return
+
+        textareaRef.current.style.height = 'auto'
+        textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`
+        setValue(event.target.value)
+    }
+
     return (
         <Reorder.Item as='div' className={container} value={item}>
-            <FormField name={item.id}>
+            <div className={dragHandle}>
+                <RowSpacingIcon />
+            </div>
+            <FormField className={formField} name={item.id}>
                 <FormControl asChild>
-                    <input />
+                    <textarea
+                        ref={textareaRef}
+                        className={textarea}
+                        rows={1}
+                        value={value}
+                        onChange={onChangeHandler}
+                    />
                 </FormControl>
             </FormField>
         </Reorder.Item>
