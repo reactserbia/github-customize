eaa822efb0dc3e0821b908ae6b9d8f5e8b4fbaf2
Mirko Basic <bejzik8@gmail.com>
2023-11-05 04:18:26 +0100

    Updated ReorderItem component.

--- src/components/ReorderItem/index.tsx
+++ src/components/ReorderItem/index.tsx
@@ -1,15 +1,17 @@
 import { Reorder } from 'framer-motion'
 
+import { Element } from '@/models'
+
 import { container } from './reorder-item.css'
 
 type ReorderItemProps = {
-    item: string
+    item: Element
 }
 
 export const ReorderItem = ({ item }: ReorderItemProps) => {
     return (
-        <Reorder.Item as='div' className={container} value={item} id={item}>
-            {item}
+        <Reorder.Item as='div' className={container} value={item}>
+            {item.id} - {item.type}
         </Reorder.Item>
     )
 }
