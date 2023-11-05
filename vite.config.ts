bb019a2ad2de18dd9e61c08a6fbce748dd9845a1
Mirko Basic <bejzik8@gmail.com>
2023-11-05 01:27:09 +0100

    Added absolute imports support.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vanillaExtractPlugin()]
+    plugins: [react(), vanillaExtractPlugin()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    }
 })
