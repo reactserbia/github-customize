c986e9664e0cdaecfedd8c382f48c28c59cce000
Mirko Basic <bejzik8@gmail.com>
2023-11-05 03:32:32 +0100

    Added buttons to the App page.

--- src/routes/app/app.css.ts
+++ src/routes/app/app.css.ts
@@ -5,6 +5,30 @@ export const container = style({
     width: '100%'
 })
 
+export const heading = style({
+    lineHeight: 1,
+    fontSize: '3rem',
+    fontWeight: 700,
+    color: vars.textContrastHigh,
+    margin: 0
+})
+
+export const subheading = style({
+    lineHeight: 1,
+    fontSize: '1.5rem',
+    fontWeight: 400,
+    color: vars.textContrastHigh,
+    marginBottom: '2rem'
+})
+
+export const buttonsContainer = style({
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: '1rem',
+    marginBottom: '1rem'
+})
+
 export const reorderItem = style({
     width: '100%',
     backgroundColor: vars.elementBackground,
--- src/routes/app/index.tsx
+++ src/routes/app/index.tsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 import { Reorder } from 'framer-motion'
 
+import { PlusIcon } from '@radix-ui/react-icons'
+
 import { ReorderItem } from '@/components'
 
-import { container } from './app.css'
+import { container, heading, subheading, buttonsContainer } from './app.css'
 
 export function App() {
-    const [items, setItems] = useState(['1', '2', '3', '4', '5'])
+    const [items, setItems] = useState<string[]>([])
 
     return (
         <div className={container}>
-            <p>WELCOME TO GITHUB ENHANCER!</p>
+            <h1 className={heading}>GitHub Enhancer</h1>
+            <h2 className={subheading}>
+                Let's create an amazing README for your profile!
+            </h2>
 
+            <div className={buttonsContainer}>
+                <button>
+                    <PlusIcon /> Heading
+                </button>
+                <button>
+                    <PlusIcon /> Subheading
+                </button>
+                <button>
+                    <PlusIcon /> Paragraph
+                </button>
+            </div>
             <Reorder.Group axis='y' values={items} onReorder={setItems}>
                 {items.map(item => (
                     <ReorderItem key={item} item={item} />
